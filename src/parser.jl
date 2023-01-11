4bf12b8e99e2b0f4e0ad87388f5c12716c1b5b22
HaeHwan <0210hwan@gmail.com>
2023-01-11 19:31:00 +0900

    Update parser.jl
    return empty dictionary when input was empty python style dict

--- src/parser.jl
+++ src/parser.jl
@@ -20,30 +20,34 @@ Returns
 result : Dictionary
 """
 function parse_nested_dict(string::String)
-    # external dictionary: get column index
-    string = String(collect(string)[2:(length(string)-2)])
-    key_value_vector = collect(Iterators.flatten([split(x, "}, ") for x in split(string, ": {")]))
-    column_index = key_value_vector[1:2:length(key_value_vector)]
-    column_index = [parse(Int64, index) for index in column_index]
+    if string == "{}"
+        return(Dict())
+    else
+        # external dictionary: get column index
+        string = String(collect(string)[2:(length(string)-2)])
+        key_value_vector = collect(Iterators.flatten([split(x, "}, ") for x in split(string, ": {")]))
+        column_index = key_value_vector[1:2:length(key_value_vector)]
+        column_index = [parse(Int64, index) for index in column_index]
     
-    # internal dictionary
-    internal_dict = []
-    nested_dicts = key_value_vector[2:2:length(key_value_vector)]
-    for dict in nested_dicts
-        dict = split(dict, ": ")
-        key_value_vector1 = collect(Iterators.flatten([rsplit(x, ", ", limit = 2) for x in dict[1:length(dict)-1]]))
-        key_value_vector2 = [dict[length(dict)]]
-        nested_key_value_vector = [key_value_vector1; key_value_vector2]
+        # internal dictionary
+        internal_dict = []
+        nested_dicts = key_value_vector[2:2:length(key_value_vector)]
+        for dict in nested_dicts
+            dict = split(dict, ": ")
+            key_value_vector1 = collect(Iterators.flatten([rsplit(x, ", ", limit = 2) for x in dict[1:length(dict)-1]]))
+            key_value_vector2 = [dict[length(dict)]]
+            nested_key_value_vector = [key_value_vector1; key_value_vector2]
         
-        nested_keys = [recover_type(k) for k in nested_key_value_vector[1:2:length(nested_key_value_vector)]]
-        nested_values = nested_key_value_vector[2:2:length(nested_key_value_vector)]
-        nested_values = [
-            value[1] == '[' ? [recover_type(v) for v in split(String(collect(value)[2:(length(value)-1)]), ", ")] :
-            [recover_type(value)] for value in nested_values
-        ]
-        push!(internal_dict, Dict(zip(nested_keys, nested_values)))
+            nested_keys = [recover_type(k) for k in nested_key_value_vector[1:2:length(nested_key_value_vector)]]
+            nested_values = nested_key_value_vector[2:2:length(nested_key_value_vector)]
+            nested_values = [
+                value[1] == '[' ? [recover_type(v) for v in split(String(collect(value)[2:(length(value)-1)]), ", ")] :
+                [recover_type(value)] for value in nested_values
+            ]
+            push!(internal_dict, Dict(zip(nested_keys, nested_values)))
+        end
+        return(Dict(zip(column_index, internal_dict)))
     end
-    return(Dict(zip(column_index, internal_dict)))
 end
 
 
